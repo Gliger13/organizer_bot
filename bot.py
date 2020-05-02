0f84568c7c45b07afe7d930f297935a1e1c49f56
andrewD46 <den46den46@gmail.com>
2020-05-02 17:43:20 +0300

    Message design update
    add the /help command
    change the style of bot messages

--- bot.py
+++ bot.py
@@ -35,7 +35,17 @@ async def start_message(message):
                            f"Напиши команду /help, чтобы увидеть доступные команды!", parse_mode='html')
 
 
-@dp.message_handler(commands=['create_task'])  # Функция отвечает на команду 'create_task'
+@dp.message_handler(commands=['help'])  # Функция отвечает на команду 'start'
+async def start_message(message):
+    await bot.send_message(message.chat.id,
+                           f"Список доступных команд:\n"
+                           f"/create - создать новое напоминание\n"
+                           f"/remove - удалить напоминание\n"
+                           f"/show - показать список всех напоминаний\n"
+                           f"/help - вывести список доступных команд\n", parse_mode='html')
+
+
+@dp.message_handler(commands=['create'])  # Функция отвечает на команду 'create'
 async def create_task(message):
     await bot.send_message(message.chat.id,
                            "Напишите время в формате HH:MM")
@@ -43,15 +53,15 @@ async def create_task(message):
     state = 'create_task_1'
 
 
-@dp.message_handler(commands=['remove_task'])  # Функция отвечает на команду 'remove_task'
+@dp.message_handler(commands=['remove'])  # Функция отвечает на команду 'remove_task'
 async def remove_task(message):
     await show_tasks(message)
-    await bot.send_message(message.chat.id, 'Напишите номер дела в списке для удаления, например "/удали_дело 2"')
+    await bot.send_message(message.chat.id, 'Напишите номер дела в списке для удаления')
     global state
     state = 'remove_task'
 
 
-@dp.message_handler(commands=['show_tasks'])  # Функция отвечает на команду 'show_tasks'
+@dp.message_handler(commands=['show'])  # Функция отвечает на команду 'show_tasks'
 async def show_tasks(message):
     user_id = message.from_user.id
     current_org = organizer.Organizer(user_id)
@@ -67,17 +77,17 @@ async def remove_task(message):
     global state_time
     if state == 'create_task_1':
         state_time[user_id] = message.text
-        await bot.send_message(message.chat.id, 'Введите ваше событие')
+        await bot.send_message(message.chat.id, 'Введите текст напоминания')
         state = 'create_task_2'
     elif state == 'create_task_2':
         description = message.text
         current_org.create_task(state_time[user_id], description)
-        await bot.send_message(message.chat.id, 'Я успешно добавил дело в список')
+        await bot.send_message(message.chat.id, 'Напоминание успешно добавлено!')
         await run_task(message, state_time[user_id], description)
     elif state == 'remove_task':
         time_index = message.text
         current_org.remove_task(int(time_index))
-        await bot.send_message(message.chat.id, 'Я удалил дело')
+        await bot.send_message(message.chat.id, 'Напоминание успешно удалено!')
         state = None
 
 
@@ -88,7 +98,7 @@ async def run_task(message, time, description):
         await bot.send_message(message.chat.id, 'Я тебя не понял. Напишите время в формате HH:MM.')
         return
     await asyncio.sleep(sec_left)
-    await bot.send_message(message.chat.id, 'Напоминаю!\n' + description)
+    await bot.send_message(message.chat.id, 'Новое напоминание:\n' + description)
 
 
 if __name__ == '__main__':
