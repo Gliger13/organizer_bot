b0c078b65f3dffa25afadecfb55d6782d4fe3d8c
Gliger13 <zanevskiyandrey@gmail.com>
2020-05-02 13:52:32 +0300

    Add bot 0.0.0

---
+++ README.md
@@ -0,0 +1,21 @@
+# In Time On Time Bot
+
+intimeontime_bot - python telegram bot that allows the user to create tasks with time reminders. Bot can:
+  - create task
+  - remove task
+  - sends the user a task message at the scheduled time
+  - show active tasks
+
+# Author
+Made by:
+- Andrey Zanevskiy (@Gliger13), Belarus
+- Martirosov Sergey (@Sergomarti), Belarus
+- Andrew Denisevich (@andrewD46), Belarus
+
+as homework for ItAcademy
+
+# License
+
+**MIT**
+
+**Free Software**
---
+++ bot.py
@@ -0,0 +1,15 @@
+import telebot
+import config
+
+bot = telebot.TeleBot(config.TOKEN)
+
+
+@bot.message_handler(commands=['start'])  # Функция отвечает на команду 'start'
+def start_message(message):
+    bot.send_message(message.chat.id,
+                     f"Привет <b>{message.from_user.first_name}</b>!👋\nЯ бот котрый напомнит тебе о твоих делах.\n"
+                     f"Просто напиши мне что и когда тебе напомнить.", parse_mode='html')
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
